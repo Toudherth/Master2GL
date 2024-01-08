daa8bf4d0be278ad1f927ad7f6ea7bc2b4b0d316
Toudherth M <toudherthmkn@gmail.com>
2024-01-08 22:47:42 +0100

    add spoon modification

--- HAI913I_Evolution_Logiciel/TP3_Logging/SpringmongoBackend/src/main/java/com/example/demo/controller/ProductController.java
+++ HAI913I_Evolution_Logiciel/TP3_Logging/SpringmongoBackend/src/main/java/com/example/demo/controller/ProductController.java
@@ -1,39 +1,27 @@
 package com.example.demo.controller;
+
 import com.example.demo.entity.Product;
 import com.example.demo.entity.User;
-import com.example.demo.exception.Result;
 import com.example.demo.service.ProductService;
+import com.example.demo.exception.Result;
 import com.example.demo.service.UserService;
-import java.util.Map;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+
+
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/product")
 public class ProductController {
-
-    // l'ajout des logs
-    private static Logger priceLogger = org.slf4j.LoggerFactory.getLogger("priceLogger");
-
-    private static Logger writeLogger = org.slf4j.LoggerFactory.getLogger("writeLogger");
-
-    private static Logger readLogger = org.slf4j.LoggerFactory.getLogger("readLogger");
-
     private ProductService productService;
 
     private UserService userService;
+
 
     private static User user;
 
@@ -41,18 +29,17 @@ public class ProductController {
      * * Constructor (injection dependences)
      */
     @Autowired
-    public ProductController(ProductService productService, UserService userService) {
+    public ProductController(ProductService productService, UserService userService ) {
         this.productService = productService;
         this.userService = userService;
     }
 
     // TODO : controller of products
     /**
-     * * Read data * Les methodes n'ont pas des logs et uniquement les methodes de produits qui auront des logs
+     * * Read data * Les methodes n'ont pas des logs et uniquement les methodes de produits qui auront des logs 
      */
     @GetMapping("/products")
     public Iterable<Product> getProducts() {
-        readLogger.info("\"message\": \"getProducts\", \"User\": \"" + user.getName() + "\", \"UserID\": \"" + user.getUserId() + "\"");;
         Iterable<Product> products = productService.listAll();
         return products;
     }
@@ -64,14 +51,9 @@ public class ProductController {
         Product product = productService.getProductById(productId);
         if (product != null) {
             boolean isMostExpensive = productService.isProductTheMostExpensive(productId);
-            Logger currentLogger = isMostExpensive ? priceLogger : readLogger;
-            if (user != null)
-                currentLogger.info("\"message\": \"getProductById\", \"User\": \"" + user.getName() + "\", \"UserID\": \"" + user.getUserId() + "\", \"Product\": \"" + product.getName() + "\", \"ProductID\": \"" + product.getProductId() + "\", \"Price\": " + product.getPrice() + "");
-
             return ResponseEntity.ok(product);
         } else
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produit non trouvé");
-
     }
 
     /**
@@ -80,7 +62,6 @@ public class ProductController {
     @PostMapping("/products")
     public ResponseEntity<Result> saveProduct(@RequestBody
     Product product) {
-        writeLogger.info("\"message\": \"saveProduct\", \"User\": \"" + user.getName() + "\", \"UserID\": \"" + user.getUserId() + "\"");;
         try {
             Result result = productService.saveProduct(product);
             if (result.isSuccess()) {
@@ -89,7 +70,8 @@ public class ProductController {
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result);
             }
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(new Result(false, "Le produit avec le même nom existe déjà."));
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(new Result(false,
+                    "Le produit avec le même nom existe déjà."));
         }
     }
 
@@ -97,7 +79,6 @@ public class ProductController {
     public ResponseEntity<Result> updateProduct(@PathVariable
     String productId, @RequestBody
     Product updatedProduct) {
-        writeLogger.info("\"message\": \"updateProduct\", \"User\": \"" + user.getName() + "\", \"UserID\": \"" + user.getUserId() + "\"");;
         Result result = productService.updateProduct(productId, updatedProduct);
         // Vérifiez le statut de la mise à jour et renvoyez la réponse correspondante
         if (result.isSuccess()) {
@@ -110,7 +91,6 @@ public class ProductController {
     @DeleteMapping("/products/{productId}")
     public ResponseEntity<?> deleteProduct(@PathVariable
     String productId) {
-        writeLogger.info("\"message\": \"deleteProduct\", \"User\": \"" + user.getName() + "\", \"UserID\": \"" + user.getUserId() + "\"");;
         try {
             productService.deleteProduct(productId);
             return ResponseEntity.ok(("Product with ID " + productId) + " deleted successfully");
